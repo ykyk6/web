960dd01631c1b4e8b70b67cd59f814690e3dbd65
ykyk6 <70152012+ykyk6@users.noreply.github.com>
2021-02-04 18:06:27 +0900

    Update quasar.js

--- src/quasar.js
+++ src/quasar.js
@@ -7,11 +7,14 @@ import lang from 'quasar/lang/zh-hant.js'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-import { Quasar } from 'quasar'
+import { Quasar, Notify } from 'quasar'
 
 Vue.use(Quasar, {
-  config: {},
+  config: {
+    notify: {}
+  },
   plugins: {
+    Notify
   },
   lang: lang,
   iconSet: iconSet
