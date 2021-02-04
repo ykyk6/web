90258753e28387f6e508f2f85293af66a476986c
ykyk6 <70152012+ykyk6@users.noreply.github.com>
2021-02-04 17:51:20 +0900

    Update matchabowlcard.vue

--- src/components/matchabowlcard.vue
+++ src/components/matchabowlcard.vue
@@ -79,22 +79,23 @@ export default {
   },
   methods: {
     addCart (cardItem) {
-      // console.log(cardItem)
-      // console.log(cardItem.image)
-      // console.log(cardItem.title)
-      console.log(cardItem.buyquantity)
-      // console.log(cardItem._id)
       if (cardItem.buyquantity.length === 0) {
         return false
       } else {
+        this.$q.notify({
+          icon: 'shopping_cart',
+          message: 'カートに入れました。',
+          color: 'grey-7',
+          position: 'top-right',
+          badgeColor: 'white',
+          timeout: 800
+        })
         const orderproducts = {}
         orderproducts.img = process.env.VUE_APP_API + '/products/file/' + cardItem.image
         orderproducts._id = cardItem._id
         orderproducts.title = cardItem.title
         orderproducts.price = cardItem.price
         orderproducts.quantity = parseInt(cardItem.buyquantity)
-        console.log(orderproducts.buyquantity)
-        // console.log(orderproducts.img)
         this.$store.commit('addCart', orderproducts)
       }
     }
@@ -104,25 +105,20 @@ export default {
       .get(process.env.VUE_APP_API + '/products/')
       .then((res) => {
         if (res.data.success) {
-          console.log(res.data.result)
           const cardItem = res.data.result.filter(function (item, index, array) {
             return item.category === 'matchabowl'
           })
           this.cardItem = cardItem
-          console.log(cardItem)
-          console.log(this.cardItem)
           // this.title = this.cardItem[0]
           const img = cardItem.map(function (item, index, array) {
             return item.image
           })
-          console.log(img)
           // this.img = img
           for (let i = 0; i < this.cardItem.length; i++) {
             console.log(img[i])
             this.img.push(process.env.VUE_APP_API + '/products/file/' + img[i])
             console.log(this.img)
           }
-          console.log(this.img)
         } else {
           this.$swal({
             icon: 'error',
@@ -146,7 +142,6 @@ export default {
 .outside{
   width: 80vw;
   height: 100vh;
-  /* background: aqua; */
 }
 .content-box{
   display: flex;
@@ -170,35 +165,23 @@ export default {
   margin-top: 5px;
 }
 .card-lorem{
-  display: block;
-flex-grow: 0.1;
-/* height: 30px; */
-/* background: darkblue; */
-margin-top: 3%;
+ display: block;
+ flex-grow: 0.1;
+ margin-top: 3%;
 }
 .text-size{
   font-family: 'Noto Sans JP', sans-serif;
   font-size: 16px;
 }
-/* .btnk{
-  background: chartreuse;
- position: absolute;
- top: 0;
- left: 0;
-} */
 .btnc{
   width: 95%;
   border: 1px solid rgba(206, 205, 205,0.8);
   margin-top: -4%;
-  /* flex-grow: 1; */
 }
 .card-price{
- /* background: darkmagenta; */
- /* display: block; */
- /* height: 20px; */
- font-size: 19px;
- margin-top: 4%;
- color: darkslategray;
+  font-size: 19px;
+  margin-top: 4%;
+  color: darkslategray;
 }
 .card-price2{
   display: block;
@@ -215,17 +198,14 @@ margin-top: 3%;
 }
 .home:hover{
   color: rgb(81, 142, 145);
-  /* text-decoration: underline;*/
 }
 .q-tab-sel:hover{
-  /* background: darkcyan; */
   border: 1px solid #6A7735;
 }
 .q-tab-sel:active{
   border: 1px solid #6A7735;
 }
 .buyqstyle{
-  /* background: #6A7735; */
   border: 1px solid rgb(214, 214, 214);
   width: 45px;
   height: 30px;
@@ -233,8 +213,6 @@ margin-top: 3%;
   margin-bottom: 5px;
 }
 .buyqtext{
-  /* background: blue; */
-  /* line-height: 20px; */
   margin-top: -8px;
   margin-right: 5px;
   font-size: 10px;
@@ -249,14 +227,11 @@ margin-top: 3%;
   .my-card {
   font-family: 'Noto Sans JP', sans-serif;
   width: 95%;
-  /* height: 290px; */
   max-width: 100%;
   height: 20%;
-  /* height: 300px; */
   display: flex;
   justify-content: center;
   padding-bottom: 5px;
-  /* background: chartreuse; */
 }
 }
 @media screen and (min-width: 600px) and (max-width: 899px) {
@@ -268,7 +243,6 @@ margin-top: 3%;
   .my-card {
   font-family: 'Noto Sans JP', sans-serif;
   width: 100%;
-  /* height: 290px; */
   max-width: 40%;
   height: 20%;
   display: flex;
@@ -285,7 +259,6 @@ margin-top: 3%;
   .my-card {
   font-family: 'Noto Sans JP', sans-serif;
   width: 100%;
-  /* height: 290px; */
   max-width: 30%;
   height: 25%;
   display: flex;
@@ -303,7 +276,6 @@ margin-top: 3%;
   .my-card {
   font-family: 'Noto Sans JP', sans-serif;
   width: 100%;
-  /* height: 290px; */
   max-width: 23%;
   height: 30%;
   display: flex;
